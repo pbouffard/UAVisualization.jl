a97eb01807e632adb2f93a0ffc7869fcafbebaf0
Patrick Bouffard <pat.bouffard@airbus-sv.com>
2018-01-17 11:31:29 -0800

    Minimal example rendering a mesh

--- src/UAVisualization.jl
+++ src/UAVisualization.jl
@@ -1,4 +1,15 @@
 module UAVisualization
+using Makie, GLVisualize
+
+export testscene
+
+function testscene()
+	scene = Scene(resolution = (500, 500))
+	mesh(GLVisualize.loadasset("cat.obj"))
+	r = linspace(-0.1, 1, 4)
+	center!(scene)
+	scene
+end
 
 # package code goes here
 
