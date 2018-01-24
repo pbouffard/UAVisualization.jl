88c1d0b5606cca2eefa5efc99b453a3f49bb8dac
Patrick Bouffard <pat.bouffard@airbus-sv.com>
2018-01-24 08:28:56 -0800

    Flesh out README, display current time from play slider. Attempt to rotate vehicle axes (not working)

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # UAVisualization
+
+Work in progress! Aims to be a generic tool for 3D visualization of flight
+trajectories of UAVs. Initial functionality will be geared towards log data
+review (realtime playback and 'scrubbing' as in digital audio/video). Vehicle
+geometry will be configurable, with a certain amount of kinematics to allow for,
+e.g., varying types of tilt-wing vehicles.
--- src/UAVisualization.jl
+++ src/UAVisualization.jl
@@ -1,6 +1,6 @@
 module UAVisualization
 using GLVisualize, GeometryTypes, Colors
-import GLVisualize: mm, Screen, labeled_slider, play_slider, center!
+import GLVisualize: mm, Screen, labeled_slider, play_slider, center!, rotationmatrix_z
 using Geodesy, MAT
 
 export init, main
@@ -13,21 +13,21 @@ function init()
 	window
 end
 
-function rgbaxis()
+function rgbaxis(rot)
 	scale = 10
 	origin = Point3f0(0, 0, 0)
 	ptx = origin + Point3f0(scale, 0, 0)
 	pty = origin + Point3f0(0, scale, 0)
 	ptz = origin + Point3f0(0, 0, scale)
 	colors = RGBA{Float32}[
-        RGBA{Float32}(1,0,0,1),
-        RGBA{Float32}(1,0,0,1),
-
-        RGBA{Float32}(0,1,0,1),
-        RGBA{Float32}(0,1,0,1),
-
-        RGBA{Float32}(0,0,1,1),
-        RGBA{Float32}(0,0,1,1),
+	RGBA{Float32}(1,0,0,1),
+	RGBA{Float32}(1,0,0,1),
+	
+	RGBA{Float32}(0,1,0,1),
+	RGBA{Float32}(0,1,0,1),
+	
+	RGBA{Float32}(0,0,1,1),
+	RGBA{Float32}(0,0,1,1),
 	]
 	lines = [origin, ptx, origin, pty, origin, ptz]
 	return visualize(lines, :linesegment, color=colors)
@@ -38,7 +38,7 @@ function loaddata()
 	vars = matread(datafile)
 	alt = vars["alt"]
 	#@show length(alt)
-	alt_nans = isnan(alt)
+	alt_nans = isnan.(alt)
 	alt = alt[.!alt_nans]
 	#@show length(alt)
 	t = vars["t"][.!alt_nans]
@@ -46,46 +46,60 @@ function loaddata()
 	lon = rad2deg.(vars["lon"][.!alt_nans])
 	lla = LLA.(lat, lon, alt)
 	trans = ENUfromLLA(lla[1], wgs84)
+	hdg = vars["hdg"][.!alt_nans]
 	enu = trans.(lla)
-	return t - t[1], enu
+	return t - t[1], enu, hdg
 end
 
 function main()
-
-	t, enu = loaddata()
-	# @show t
-	# @show enu
-
-	pos = i -> begin
-		@show t[i]
-		foo = Point3f0(enu[i]...)
+	
+	t, enu, hdg = loaddata()
+	
+	"Vehicle's position in ENU at a given index"
+	function get_pos_enu(i)
+		Point3f0(enu[i]...)
 	end
 
+	"Vehicle's rotatation at a given index"
+	function get_rot(i)
+		rotationmatrix_z(hdg[i])
+	end
+
+	
 	iconsize = 8mm
 	editarea, viewarea = x_partition_abs(window.area, round(Int, 8.2 * iconsize))
 	edit_screen = Screen(window, area = editarea)
 	view_screen = Screen(window, area = viewarea)
 	#cubecamera(view_screen)
-
-    time_viz, time_value = labeled_slider(1:1:length(t), edit_screen)
-
-	play_viz, slider_value = play_slider(edit_screen, iconsize, 1:1:length(t))
-	pos_animation = map(slider_value) do i
+	
+	time_viz, time_value = labeled_slider(1:1:length(t), edit_screen)
+	
+	# i_cur is the main Signal: Current index into data arrays using playback slider
+	play_viz, i_cur = play_slider(edit_screen, iconsize, 1:1:length(t))
+	
+	cur_rot = map(get_rot, i_cur)
+	curtime = map(i -> t[i], i_cur)
+	curtime_s = map(t -> @sprintf("%.2f", t), curtime)
+	pos_animation = map(i_cur) do i
 		#pos(Int(i))
 		#Sphere{Float32}(Point3f0(0.0), 1f0)
-		[pos(i)]
+		[get_pos_enu(i)]
 	end
-
-	controls = Pair["play" => play_viz, "time" => time_viz]
-
+	
+	#mesh = loadasset(Pkg.dir("UAVisualization") * "/tools/Canopy_v2.STL")
+	
+	controls = Pair["playback" => play_viz, "time" => curtime_s]
+	
 	vehmarker = visualize((Sphere{Float32}(Point3f0(0.0), 1f0), pos_animation), color=RGBA(1f0,0f0,0f0,1f0))
 	enutraj = visualize(Point3f0.(enu)[:], :lines, thickness=2f0, line=4.0)
-
+	
+	#_view(visualize(mesh), view_screen, camera = :perspective)
 	_view(vehmarker, view_screen, camera = :perspective)
 	_view(visualize(controls, text_scale = 4mm, width = 8*iconsize), edit_screen, camera = :fixed_pixel)
 	_view(enutraj, view_screen, camera = :perspective)
-	_view(rgbaxis(), view_screen, camera = :perspective)
-
+	_view(rgbaxis(cur_rot), view_screen, camera = :perspective)
+	
+	push!(view_screen.cameras[:perspective].nearclip, 0.02f0)
 	center!(view_screen, :perspective)
 	renderloop(window)
 end
